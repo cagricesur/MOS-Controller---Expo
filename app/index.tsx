f6243deab853c21cb8ae85082e9cc98c6812d231
Çağrı Cesur <cagri.cesur@veripark.com>
2024-06-23 09:38:48 +0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,67 +1,32 @@
-import * as SplashScreen from "expo-splash-screen";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState } from "react";
 import { ActivityIndicator, SafeAreaView } from "react-native";
 import { WebView } from "react-native-webview";
 
-SplashScreen.preventAutoHideAsync();
 export default function App() {
-  const [counter, setCounter] = useState<number>(5);
   const [isLoading, setLoading] = useState<boolean>(true);
-  const onLayoutRootView = useCallback(async () => {
-    if (counter === 0) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      await SplashScreen.hideAsync();
-    }
-  }, [counter]);
 
-  let interval: NodeJS.Timeout | undefined;
-  useEffect(() => {
-    if (!interval) {
-      interval = setInterval(() => {
-        setCounter(counter - 1);
-      }, 1000);
-    }
-    if (counter === 0) {
-      interval && clearInterval(interval);
-    }
-    return () => {
-      interval && clearInterval(interval);
-    };
-  }, [counter]);
-
-  if (counter > 0) {
-    return null;
-  } else {
-    return (
-      <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
-        {isLoading && (
-          <ActivityIndicator
-            color="#ff0000"
-            style={{
-              position: "absolute",
-              left: 0,
-              right: 0,
-              bottom: 0,
-              top: 0,
-              zIndex: 99,
-            }}
-            size={64}
-          />
-        )}
-        <WebView
-          onLoadProgress={(event) => {}}
-          onLoadStart={() => {}}
-          onLoad={() => {}}
-          onLoadEnd={() => {
-            setLoading(false);
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      {isLoading && (
+        <ActivityIndicator
+          color="#ff0000"
+          style={{
+            position: "absolute",
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            zIndex: 99,
           }}
-          source={{ uri: `${process.env.EXPO_PUBLIC_WEBVIEW_URL}` }} // Replace with your website URL
+          size={64}
         />
-      </SafeAreaView>
-    );
-  }
+      )}
+      <WebView
+        onLoadEnd={() => {
+          setLoading(false);
+        }}
+        source={{ uri: `${process.env.EXPO_PUBLIC_WEBVIEW_URL}` }} // Replace with your website URL
+      />
+    </SafeAreaView>
+  );
 }
